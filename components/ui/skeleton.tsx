4f23a883c34df8fc1a3cfd477d0f5ee07ec81e5e
Victor Herrera <vhherrera98@gmail.com>
2025-10-28 15:45:33 -0400

    fixed skeleton

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -4,7 +4,7 @@ function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="skeleton"
-      className={cn("bg-gray-400 dark:bg-accent animate-pulse rounded-md", className)}
+      className={cn("bg-gray-400 dark:bg-background animate-pulse rounded-md", className)}
       {...props}
     />
   )
